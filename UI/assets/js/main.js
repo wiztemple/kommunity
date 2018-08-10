9ceb7cdc3cc7fd06d320169177a531049a740cfe
wiztemple <wiztemple7@gmail.com>
2018-08-10 04:52:09 +0100

    feat(UI): add landing page
    - add add question page
    
    - add answer page
    
    - add a user profile page
    
    - add signup page
    
    - add signin page
    
    [finishes #159608044 #159646309 #159646339 #159646263 #159608204 #159608255]

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -2,9 +2,10 @@ function App() {
     const modal = document.getElementById('modal');
     const modalBtn = document.getElementById('modalBtn');
     const closeBtn = document.querySelector('.closeBtn');
-    const cardBtn = document.querySelector('.card-btn');
+    const cardBtn = document.getElementById('cardBtn');
+    const commentBtn = document.getElementById('commentTag');
     const bar = document.querySelector('.navbar__nav-toggle');
-
+   
     this.openModal = () => modal.style.display = 'block';
     this.closeModal = () => modal.style.display = 'none';
     this.clickOutside = (e) => {
@@ -12,25 +13,34 @@ function App() {
            modal.style.display = 'none';
        }
     }
-    this.dropDown = () => {
-       const answerBox = document.querySelector('.answer-box');
-       if (answerBox.style.display === 'none') {
+    this.dropAnswerBox = () => {
+       const answerBox = document.getElementById('answer-box');
+       if (answerBox.style.display !== 'block') {
            answerBox.style.display = 'block';
        } else {
            answerBox.style.display = 'none';
        }
     }
+    this.dropCommentBox = () => {
+        const answerBox = document.getElementById('comment-box');
+        if (answerBox.style.display !== 'block') {
+            answerBox.style.display = 'block';
+        } else {
+            answerBox.style.display = 'none';
+        }
+     }
     this.toggle = () => {
         const navs = document.querySelector('.nav__items');
         navs.forEach(nav => {
             nav.classList.add('navbar__toggleShow');
-        })
+        });
     }
-    cardBtn.addEventListener('click', this.dropDown);
+    cardBtn.addEventListener('click', this.dropAnswerBox);
+    commentBtn.addEventListener('click', this.dropCommentBox);
     modalBtn.addEventListener('click', this.openModal);
     closeBtn.addEventListener('click', this.closeModal);
-    bar.addEventListener('click', this.toggle);
     window.addEventListener('click', this.clickOutside);
+    bar.addEventListener('click', this.toggle);
 }
 const appInst = new App();
 appInst();
