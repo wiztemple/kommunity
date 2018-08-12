f7a043c83567711e50f934731f9cc4347a96af90
wiztemple <wiztemple7@gmail.com>
2018-08-12 12:30:53 +0100

    font-style change

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -1,46 +1,46 @@
-function App() {
-    const modal = document.getElementById('modal');
-    const modalBtn = document.getElementById('modalBtn');
-    const closeBtn = document.querySelector('.closeBtn');
-    const cardBtn = document.getElementById('cardBtn');
-    const commentBtn = document.getElementById('commentTag');
-    const bar = document.querySelector('.navbar__nav-toggle');
-   
-    this.openModal = () => modal.style.display = 'block';
-    this.closeModal = () => modal.style.display = 'none';
-    this.clickOutside = (e) => {
-       if (e.target === modal) {
-           modal.style.display = 'none';
-       }
-    }
-    this.dropAnswerBox = () => {
-       const answerBox = document.getElementById('answer-box');
-       if (answerBox.style.display !== 'block') {
-           answerBox.style.display = 'block';
-       } else {
-           answerBox.style.display = 'none';
-       }
-    }
-    this.dropCommentBox = () => {
-        const answerBox = document.getElementById('comment-box');
-        if (answerBox.style.display !== 'block') {
-            answerBox.style.display = 'block';
-        } else {
-            answerBox.style.display = 'none';
-        }
-     }
-    this.toggle = () => {
-        const navs = document.querySelector('.nav__items');
-        navs.forEach(nav => {
-            nav.classList.add('navbar__toggleShow');
-        });
-    }
-    cardBtn.addEventListener('click', this.dropAnswerBox);
-    commentBtn.addEventListener('click', this.dropCommentBox);
-    modalBtn.addEventListener('click', this.openModal);
-    closeBtn.addEventListener('click', this.closeModal);
-    window.addEventListener('click', this.clickOutside);
-    bar.addEventListener('click', this.toggle);
+// function App() {
+const modal = document.getElementById('modal');
+const modalBtn = document.getElementById('modalBtn');
+const closeBtn = document.querySelector('.closeBtn');
+const bar = document.querySelector('.navbar__nav-toggle');
+const cardBtn = document.getElementById('cardBtn');
+const commentTag = document.getElementById('commentTag');
+
+eventListener();
+
+function eventListener() {
+  modalBtn.addEventListener('click', openModal);
+  closeBtn.addEventListener('click', closeModal);
+  bar.addEventListener('click', navToggle);
+  cardBtn.addEventListener('click', dropAnswerBox);
+  commentTag.addEventListener('click', dropCommentBox);
 }
-const appInst = new App();
-appInst();
+
+function openModal() {
+    modal.style.display = 'block';
+}
+function closeModal() {
+    modal.style.display = 'none';
+}
+function navToggle() {
+    const navs = document.querySelector('.nav__items');
+    console.log(navs);
+    navs.forEach(nav => nav.classList.add('navbar__toggleShow')); 
+}
+function dropAnswerBox() {
+    const answerBox = document.getElementById('answerBox');
+    if (answerBox.style.display !== 'block') {
+        answerBox.style.display = 'block';
+    }
+    answerBox.style.display = 'none';
+    
+}
+function dropCommentBox() {
+    const commentBox = document.getElementById('commentBox');
+    if (commentBox.style.display !== 'block') {
+        commentBox.style.display = 'block';
+    }
+    commentBox.style.display = 'none';
+}
+
+
