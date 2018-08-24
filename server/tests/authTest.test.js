3f0491db25e16f8d18b36ea0f400dc116cb06116
wiztemple <wiztemple7@gmail.com>
2018-08-24 13:16:21 +0100

    ch(test)

--- server/tests/authTest.test.js
+++ server/tests/authTest.test.js
@@ -8,129 +8,127 @@ chai.should();
 const email = Math.random().toString(36).substring(2, 15);
 const username = Math.random().toString(36).substring(2, 15);
 
-describe('Authentication', () => {
-  describe('User Account Creation', () => {
-    it('should create account if the required fields are provided', (done) => {
-      const user = {
-        username,
-        email: `${email}@gmail.com`,
-        password: 'lastmanstanding'
-      };
-      chai.request(app)
-        .post('/api/v1/auth/signup')
-        .send(user)
-        .end((error, response) => {
-          response.should.have.status(201);
-          response.should.be.an('object');
-          response.should.have.property('message').to.equal('User successfully created');
-          done();
-        });
-    });
-    it('should not create account if the user already exists', (done) => {
-      const user = {
-        username: 'Sullivan',
-        email: 'wiztemple@gmail.com',
-        password: 'lastmanstanding',
-      };
-      chai.request(app)
-        .post('/api/v1/auth/signup')
-        .send(user)
-        .end((error, response) => {
-          response.should.have.status(409);
-          response.should.be.an('object');
-          response.should.have.property('message').to.equal('user already exists');
-          done();
-        });
-    });
-    it('should not create account if the email format is invalid', (done) => {
-      const user = {
-        username: 'Sullivan',
-        email: 'wiztemgmail.com',
-        password: 'lastdass'
-      };
-      chai.request(app)
-        .post('/api/v1/auth/signup')
-        .send(user)
-        .end((error, response) => {
-          response.should.have.status(400);
-          response.should.have.property('message').to.equal('invalid email');
-          done();
-        });
-    });
-    it('should not create account if the the provided password is not greater than 5', (done) => {
-      const user = {
-        username: 'Sullivan',
-        email: 'wiztemple@gmail.com',
-        password: 'las',
-      };
-      chai.request(app)
-        .post('/api/v1/auth/signup')
-        .send(user)
-        .end((error, response) => {
-          response.should.have.status(400);
-          response.should.have.property('message').to.equal('password must be greater than 5');
-          done();
-        });
-    });
-    it('should not create account if username field is empty', (done) => {
-      const user = {
-        username: '',
-        email: 'wiztemple@gmail.com',
-        password: 'last',
-      };
-      chai.request(app)
-        .post('/api/v1/auth/signup')
-        .send(user)
-        .end((error, response) => {
-          response.should.have.status(400);
-          response.should.have.property('message').to.equal('username is required');
-          done();
-        });
-    });
-    it('should not create account if username fcontains special character', (done) => {
-      const user = {
-        username: 'lets###',
-        email: 'wiztemple@gmail.com',
-        password: 'last',
-      };
-      chai.request(app)
-        .post('/api/v1/auth/signup')
-        .send(user)
-        .end((error, response) => {
-          response.should.have.status(400);
-          response.should.have.property('message').to.equal('username is required');
-          done();
-        });
-    });
-    it('should not create account if the email is empty', (done) => {
-      const user = {
-        username: 'wizly',
-        email: '',
-        password: 'last',
-      };
-      chai.request(app)
-        .post('/api/v1/auth/signup')
-        .send(user)
-        .end((error, response) => {
-          response.should.have.status(400);
-          response.should.have.property('message').to.equal('email is required');
-          done();
-        });
-    });
-    it('should not create account if the password field is empty', (done) => {
-      const user = {
-        username: 'wizly',
-        email: 'gmail@gmail.com',
-        password: '',
-      };
-      chai.request(app)
-        .post('/api/v1/auth/signup')
-        .send(user)
-        .end((error, response) => {
-          response.should.have.status(400);
-          response.should.have.property('message').to.equal('password is required');
-          done();
-        });
-    });
+describe('User Account Creation', () => {
+  it('should create account if the required fields are provided', (done) => {
+    const user = {
+      username,
+      email: `${email}@gmail.com`,
+      password: 'lastmanstanding'
+    };
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .end((error, response) => {
+        response.should.have.status(201);
+        response.should.be.an('object');
+        response.should.have.property('message').to.equal('User successfully created');
+        done();
+      });
+  });
+  it('should not create account if the user already exists', (done) => {
+    const user = {
+      username: 'Sullivan',
+      email: 'wiztemple@gmail.com',
+      password: 'lastmanstanding',
+    };
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .end((error, response) => {
+        response.should.have.status(409);
+        response.should.be.an('object');
+        response.should.have.property('message').to.equal('user already exists');
+        done();
+      });
+  });
+  it('should not create account if the email format is invalid', (done) => {
+    const user = {
+      username: 'Sullivan',
+      email: 'wiztemgmail.com',
+      password: 'lastdass'
+    };
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .end((error, response) => {
+        response.should.have.status(400);
+        response.should.have.property('message').to.equal('invalid email');
+        done();
+      });
+  });
+  it('should not create account if the the provided password is not greater than 5', (done) => {
+    const user = {
+      username: 'Sullivan',
+      email: 'wiztemple@gmail.com',
+      password: 'las',
+    };
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .end((error, response) => {
+        response.should.have.status(400);
+        response.should.have.property('message').to.equal('password must be greater than 5');
+        done();
+      });
+  });
+  it('should not create account if username field is empty', (done) => {
+    const user = {
+      username: '',
+      email: 'wiztemple@gmail.com',
+      password: 'last',
+    };
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .end((error, response) => {
+        response.should.have.status(400);
+        response.should.have.property('message').to.equal('username is required');
+        done();
+      });
+  });
+  it('should not create account if username fcontains special character', (done) => {
+    const user = {
+      username: 'lets###',
+      email: 'wiztemple@gmail.com',
+      password: 'last',
+    };
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .end((error, response) => {
+        response.should.have.status(400);
+        response.should.have.property('message').to.equal('username is required');
+        done();
+      });
+  });
+  it('should not create account if the email is empty', (done) => {
+    const user = {
+      username: 'wizly',
+      email: '',
+      password: 'last',
+    };
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .end((error, response) => {
+        response.should.have.status(400);
+        response.should.have.property('message').to.equal('email is required');
+        done();
+      });
+  });
+  it('should not create account if the password field is empty', (done) => {
+    const user = {
+      username: 'wizly',
+      email: 'gmail@gmail.com',
+      password: '',
+    };
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .end((error, response) => {
+        response.should.have.status(400);
+        response.should.have.property('message').to.equal('password is required');
+        done();
+      });
   });
 });
--- server/tests/questionTest.test.js
+++ server/tests/questionTest.test.js
@@ -4,105 +4,117 @@ import app from '../../app';
 
 chai.use(chaiHttp);
 chai.should();
-const userToken = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJvYmlAZ21haWwuY29tIiwiaWF0IjoxNTMyNjM2MjAyLCJleHAiOjE1MzI3MjI2MDJ9.mt16o3b7ZF0N__Wj7CLR7EumLUm5Eop1Ef5_JoeHxyw';
-const title = Math.random().toString(36).substring(2, 15);
+// const userToken = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJvYmlAZ21haWwuY29tIiwiaWF0IjoxNTMyNjM2MjAyLCJleHAiOjE1MzI3MjI2MDJ9.mt16o3b7ZF0N__Wj7CLR7EumLUm5Eop1Ef5_JoeHxyw';
+// const title = Math.random().toString(36).substring(2, 15);
+let userToken;
 
-describe('Authentication', () => {
-  describe('POST Question', () => {
-    it('should post question if all inputs are provided', (done) => {
-      const user = {
-        title,
-        questionBody: 'lorem ipsum drum grity',
-        tag: 'C++'
-      };
-      chai.request(app)
-        .post('/api/v1/question')
-        .set('Authorization', userToken)
-        .send(user)
-        .end((error, response) => {
-          response.should.have.status(201);
-          response.should.be.an('object');
-          response.should.have.property('message').to.equal('question successfully posted');
-          done();
-        });
-    });
-    it('should not post question if the title field is empty', (done) => {
-      const user = {
-        title: '',
-        questionBody: 'lorem ipsum drum grity',
-        tag: 'C++'
-      };
-      chai.request(app)
-        .post('/api/v1/question')
-        .set('Authorization', userToken)
-        .send(user)
-        .end((error, response) => {
-          response.should.have.status(400);
-          response.should.be.an('object');
-          response.should.have.property('message').to.equal('title is required');
-          done();
-        });
-    });
-    it('should not post question if the questionBody field is empty', (done) => {
-      const user = {
-        title: 'lorem drum',
-        questionBody: '',
-        tag: 'C++'
-      };
-      chai.request(app)
-        .post('/api/v1/question')
-        .set('Authorization', userToken)
-        .send(user)
-        .end((error, response) => {
-          response.should.have.status(400);
-          response.should.be.an('object');
-          response.should.have.property('message').to.equal('question body is required');
-          done();
-        });
-    });
+describe('POST Question', () => {
+  before((done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        username: 'sullivan',
+        email: 'sullivan@gmail.com',
+        password: 'lastdays008'
+      })
+      .end((error, response) => {
+        userToken = response.body.data.token;
+        done();
+      });
   });
-  describe('GET ALL Question', () => {
-    it('should return all questions asked', (done) => {
-      chai.request(app)
-        .get('/api/v1/question')
-        .end((error, response) => {
-          response.should.have.status(200);
-          response.should.be.an('object');
-          response.should.have.property('message').to.equal('question successfully posted');
-          done();
-        });
-    });
+  it('should post question if all inputs are provided', (done) => {
+    const user = {
+      title: 'lorem ipsum',
+      questionBody: 'lorem ipsum drum grity',
+      tag: 'anyting'
+    };
+    chai.request(app)
+      .post('/api/v1/question')
+      .set('Authorization', userToken)
+      .send(user)
+      .end((error, response) => {
+        response.should.have.status(201);
+        response.should.be.an('object');
+        response.should.have.property('message').to.equal('question successfully posted');
+        done();
+      });
   });
-  describe('GET A Question', () => {
-    it('should return all a specific question', (done) => {
-      chai.request(app)
-        .get('/api/v1/question/3')
-        .end((error, response) => {
-          response.should.have.status(200);
-          response.should.be.an('object');
-          response.should.have.property('message').to.equal('question successfully returned');
-          done();
-        });
-    });
-    it('should not return a question if Id is not a number', (done) => {
-      chai.request(app)
-        .get('/api/v1/question/yy')
-        .end((error, response) => {
-          response.should.have.status(400);
-          response.should.be.an('object');
-          response.should.have.property('message').to.equal('question id must be a number');
-          done();
-        });
-    });
-    it('should not return a question if Id does not exist', (done) => {
-      chai.request(app)
-        .get('/api/v1/question/yy')
-        .end((error, response) => {
-          response.should.have.status(404);
-          response.should.be.an('object');
-          response.should.have.property('message').to.equal('question not found');
-          done();
-        });
-    });
+  it('should not post question if the title field is empty', (done) => {
+    const user = {
+      title: '',
+      questionBody: 'lorem ipsum drum grity',
+      tag: 'C++'
+    };
+    chai.request(app)
+      .post('/api/v1/question')
+      .set('Authorization', userToken)
+      .send(user)
+      .end((error, response) => {
+        response.should.have.status(400);
+        response.should.be.an('object');
+        response.should.have.property('message').to.equal('title is required');
+        done();
+      });
+  });
+  it('should not post question if the questionBody field is empty', (done) => {
+    const user = {
+      title: 'lorem drum',
+      questionBody: '',
+      tag: 'C++'
+    };
+    chai.request(app)
+      .post('/api/v1/question')
+      .set('Authorization', userToken)
+      .send(user)
+      .end((error, response) => {
+        response.should.have.status(400);
+        response.should.be.an('object');
+        response.should.have.property('message').to.equal('question body is required');
+        done();
+      });
+  });
+});
+describe('GET ALL Question', () => {
+  it('should return all questions asked', (done) => {
+    chai.request(app)
+      .get('/api/v1/question')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.an('object');
+        response.should.have.property('message').to.equal('question successfully posted');
+        done();
+      });
+  });
+});
+describe('GET A Question', () => {
+  it('should return all a specific question', (done) => {
+    chai.request(app)
+      .get('/api/v1/question/3')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.an('object');
+        response.should.have.property('message').to.equal('question successfully returned');
+        done();
+      });
+  });
+  it('should not return a question if Id is not a number', (done) => {
+    chai.request(app)
+      .get('/api/v1/question/yy')
+      .end((error, response) => {
+        response.should.have.status(400);
+        response.should.be.an('object');
+        response.should.have.property('message').to.equal('question id must be a number');
+        done();
+      });
+  });
+  it('should not return a question if Id does not exist', (done) => {
+    chai.request(app)
+      .get('/api/v1/question/yy')
+      .end((error, response) => {
+        response.should.have.status(404);
+        response.should.be.an('object');
+        response.should.have.property('message').to.equal('question not found');
+        done();
+      });
   });
 });
