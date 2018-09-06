f6b18820b38b257b1ca3d880abbb914d91438c33
wiztemple <wiztemple7@gmail.com>
2018-09-06 14:07:32 +0100

    ft(signup)
    - use fetch to consume signup endpoint
    
    - test from the signup page
    
    - [Finishes #160267357]

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import logger from 'volleyball';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import path from 'path';
 import authRoute from './server/routes/authRoute';
 import questionRoute from './server/routes/questionRoute';
 
@@ -15,6 +17,12 @@ app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors());
+
+app.get('/', (request, response) => response.sendfile('./client/index.html'));
+
+// connect static files
+app.use(express.static(path.resolve(__dirname, './client/')));
 app.get('/', (request, response) => response.status(200).json({
   status: 'success',
   message: 'Welcome to Kommunity, a platform to ask questions and answer questions',
