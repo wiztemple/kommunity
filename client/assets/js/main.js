0c6212ff3dc66297c91aaef2c6af7eede8c5f8f3
wiztemple <wiztemple7@gmail.com>
2018-09-09 19:00:42 +0100

    ft(fetch-all-questions)
    - fetch all questions from the all questions endpoint
    
    [Finishes #160325777]

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,5 +1,5 @@
 const bar = document.querySelector('.navbar__nav-toggle');
-const ellipis = document.querySelector('.card-hide');
+
 
 function navToggle() {
   const navs = document.querySelector('.nav__items');
@@ -7,12 +7,3 @@ function navToggle() {
 }
 bar.addEventListener('click', navToggle);
 
-function dropEllips() {
-  const cardDrop = document.querySelector('.card-drop');
-  if (cardDrop.style.display !== 'block') {
-   cardDrop.style.display = 'block';
-  } else {
-    cardDrop.style.display = 'none';
-  }
-}
-ellipis.addEventListener('click', dropEllips);
